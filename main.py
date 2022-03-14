c21226d4f3c5bc8c37c7fead25a2b1de2b169eca
Alexandre D <93094416+adcrry@users.noreply.github.com>
2022-03-14 17:55:34 +0100

    plein de chose

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ from skimage.transform import radon, rescale, iradon, rotate
 import math as math;
 import numpy as np
 import imageio 
+import scipy.signal
+
 #obj = io.imread('disque.jpg')
-obj = imageio.imread("original.png")
+obj = imageio.imread("radio_filtree.png")
 xlen = len(obj)
 ylen = len(obj[0])
     
@@ -75,6 +77,7 @@ def sinogram(obj):
     M = 180
     projections = np.zeros((M, obj.shape[0]))
     for k in range(-int(xlen/2), int(xlen/2)):
+        print(k)
         for m in range(0, M):
             projections[m, -int(xlen/2)+k] = (R__(obj, m * math.pi / M, k))
     
@@ -82,8 +85,18 @@ def sinogram(obj):
     plt.show()
     #plt.imshow(np.vstack(projections))
     #plt.show()
-    return rotate(projections,-180)
+    return projections
     
+def sinogram_(obj, steps):
+    projections = []       
+    dTheta = -180.0 / steps 
+    
+    for i in range(steps):
+        projections.append(rotate(obj, i*dTheta).sum(axis=0))
+    
+    return np.vstack(projections) 
+    
+
 def value(tab, i, j):
     if i >= 0 and i < np.shape(tab)[0] and j >= 0 and j < np.shape(tab)[1]:
         return tab[i,j];
@@ -91,7 +104,7 @@ def value(tab, i, j):
         return 0;
 
 
-def reverse(im):
+def retroprojection(im):
     xlen = im.shape[0]
     ylen = im.shape[1]
     resultat = np.zeros((ylen, ylen))
@@ -102,11 +115,23 @@ def reverse(im):
         resultat += temp
     return resultat
 
+def resize_reversed(im):
+    d = len(im)//4
+    return im[d:3*d, d:3*d]
+
 def Lambda(N, M):
     t = np.zeros((N, M))
     for k in range(N):
-        t[k, :] = abs(math.pi * np.fft.fftfreq(M, 2 / N))
+        t[k, :] = abs(math.pi * np.fft.fftfreq(M, 2 /  M))
     return t
+
+def hann_filter(N,M):
+    t = np.zeros((N, M))
+    for k in range(N):
+        t[k, :] = abs(math.pi * np.fft.fftfreq(M, 2 / N))
+    return t*(np.fft.fftshift(np.hanning(M)))
+
+
 
 def Q(im, N, M):
     A = Lambda(N, M)  
@@ -114,16 +139,67 @@ def Q(im, N, M):
     C = A * B  
     return np.fft.ifft(C, axis=1)  
 
+def Q2(im, N, M):
+    A = hann_filter(N, M)  
+    B = np.fft.fft(im, axis=1)  
+    C = A * B  
+    return np.fft.ifft(C, axis=1)  
 
 def reverse2(im):
     xlen = im.shape[0]
     ylen = im.shape[1]
     im = Q(im, xlen, ylen).real  
-    resultat = np.zeros((ylen, ylen))
-    dTheta = 180.0 / xlen
-    im = rotate(im, -90)
-    for i in range(xlen):
-        temp = np.tile(im[i], (ylen, 1))
-        temp = rotate(temp, dTheta * i)
-        resultat += temp
-    return resultat
+    return retroprojection(im)
+    
+
+#Filtrage rampe + lissant
+def reverse_hann(im):
+    xlen = im.shape[0]
+    ylen = im.shape[1]
+    im = Q2(im, xlen, ylen).real  
+    return retroprojection(im)
+
+
+def g(x):
+    return (int(255.*(1.+np.sin(x*np.pi/255. - np.pi/2.))/2.))
+
+X_g = np.linspace(0,255,255);
+Y_g = []
+for k in X_g:
+    Y_g.append(g(k))
+plt.plot(X_g, Y_g)
+
+
+def applyg(img):
+    n,m = np.shape(img)[0], np.shape(img)[1]
+    result = np.zeros((n,m))
+    for i in range(n):
+        for j in range(m):
+            result[i,j] = g(img[i,j])
+            
+    return result;
+
+def porte(img):
+    n,m = np.shape(img)[0], np.shape(img)[1]
+    result = np.zeros((n,m))
+    for i in range(n):
+        for j in range(m):
+            if img[i,j] < 100:
+                result[i,j] = 0
+            else:
+                result[i,j] = img[i,j]
+    return result;
+
+###############################
+#Utilitaire Plot
+###############################
+
+def drawSino(im):
+    plt.imshow(im)
+    #plt.colorbar()
+    #plt.title('Titre', fontsize=8)
+    plt.xlabel(r"$\rho$ (en pixel)")
+    plt.ylabel(r"$\theta$ (en degré)")
+    plt.figure(1, figsize=(354/27,252/27), dpi=27)
+    plt.savefig("fig_test.png")
+    plt.show()
